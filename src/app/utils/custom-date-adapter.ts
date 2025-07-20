6176975f4d1972127167df33562c6e2901cf5010
daothannhangian <binhk4a@ gmail.com>
2025-07-20 18:54:01 +0700

    chat

--- src/app/utils/custom-date-adapter.ts
+++ src/app/utils/custom-date-adapter.ts
@@ -4,6 +4,7 @@ import moment from "moment";
 export class CustomDateAdapter extends NativeDateAdapter {
   override format(date: Date, displayFormat: Object): string {
     moment.locale('vi');
+
     return moment(date).format('DD/MM/YYYY');
   }
 }
